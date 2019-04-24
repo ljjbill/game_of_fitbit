0fd0bb0432da88bd24af8ea636420126a8c55843
l-gar <luis.garcia.wi.2793@gmail.com>
2019-04-23 21:47:35 -0500

    added second model to code

--- app.py
+++ app.py
@@ -39,12 +39,15 @@ from datetime import timedelta
 # IMPORT DEPENDENCIES FOR Machine Learning
 #################################################
 from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import r2_score, make_scorer
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 from scipy import stats
+import math
 import numpy as np
 import xgboost as xgb
-import math
+import pickle
 
 #################################################
 # Flask Setup
@@ -68,44 +71,43 @@ Base.prepare(db.engine, reflect=True)
 """study = Base.classes.study""" # EXAMPLE
 
 def xgboost_model(model_name, features):
-    # model_name: name of the trained xgboost model, e.g.: 'calorie_predictor.model'
-    # features: 1 x 11 features as numpy array, e.g.: np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) where the numbers are feature values
+      # model_name: name of the trained xgboost model, e.g.: 'calorie_predictor.model'
+      # features: 1 x 11 features as numpy array, e.g.: np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) where the numbers are feature values
 
-    # Loads the model saved in model_name. Runs the model using given feature vector. Returns the predicted calories.
+      # Loads the model saved in model_name. Runs the model using given feature vector. Returns the predicted calories.
 
-    # loading model
-    mod = xgb.Booster({'nthread':4})
-    mod.load_model(model_name)
+      # loading model
+      mod = xgb.Booster({'nthread':4})
+      mod.load_model(model_name)
 
-    assert np.shape(features)[0] == 11 # make sure number of features is correct
+      assert np.shape(features)[0] == 11 # make sure number of features is correct
 
-    features = features[np.newaxis, :]
-    feat = xgb.DMatrix(features)
+      features = features[np.newaxis, :]
+      feat = xgb.DMatrix(features)
 
-    return mod.predict(feat)
+      return mod.predict(feat)
 
 def svm_model(model_name, scale_name, features):
-    # model_name: name of the trained Gradient Boosted Regressor model, e.g.: 'calorie_predictor_GBR.model'
-    # scale_name: name of file containing variable to scale features
-    # features: 1 x 11 features as numpy array, e.g.: np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) where the numbers are feature values
+   # model_name: name of the trained Gradient Boosted Regressor model, e.g.: 'calorie_predictor_GBR.model'
+   # scale_name: name of file containing variable to scale features
+   # features: 1 x 11 features as numpy array, e.g.: np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) where the numbers are feature values
 
-    # Loads the model saved in model_name. Runs the model using given feature vector. Returns the predicted calories.
-
-    # loading model
-    with open(model_name, 'rb') as f:
-        calorie_model = pickle.load(f)
+   # Loads the model saved in model_name. Runs the model using given feature vector. Returns the predicted calories.
+   with open(model_name, 'rb') as f:
+      calorie_model = pickle.load(f)
 
     # loading scaler
-    with open(scale_name, 'rb') as f:
-        X_scaler = pickle.load(f)
+   with open(scale_name, 'rb') as f:
+      X_scaler = pickle.load(f)
 
-    assert np.shape(features)[0] == 11 # make sure number of features is correct
+   assert np.shape(features)[0] == 11 # make sure number of features is correct
 
-    features = features[np.newaxis, :]
-    features = X_scaler.transform(features)
+   features = features[np.newaxis, :]
+   features = X_scaler.transform(features)
 
 
-    return calorie_model.predict(features)
+   return calorie_model.predict(features)
+
 
 
 # creates a route for the flask front end
@@ -131,21 +133,22 @@ def unpack_data(d):
 # Query the database and send the jsonified results
 @app.route("/send", methods=["GET", "POST"])
 def send():
-    if request.method == "POST":
-        steps = request.form["userSteps"]
-        tra_dist = request.form["TrackerDistance"]
-        log_act_dist = request.form["LogActDist"]
-        very_act_dist = request.form["VeryActDist"]
-        mod_act_dist = request.form["ModActDist"]
-        light_act_dist = request.form["LightActDist"]
-        seden_act_dist = request.form["SedenActDist"]
-        very_act_min = request.form["VeryActMin"]
-        fair_act_min = request.form["FairActMin"]
-        light_act_min = request.form["LightActMin"]
-        seden_act_min = request.form["SedenMin"]
-        print(request.form.get("chosenModel", None))
-        d=request.form.to_dict(flat=False)
-        try:
+   if request.method == "POST":
+      steps = request.form["userSteps"]
+      tra_dist = request.form["TrackerDistance"]
+      log_act_dist = request.form["LogActDist"]
+      very_act_dist = request.form["VeryActDist"]
+      mod_act_dist = request.form["ModActDist"]
+      light_act_dist = request.form["LightActDist"]
+      seden_act_dist = request.form["SedenActDist"]
+      very_act_min = request.form["VeryActMin"]
+      fair_act_min = request.form["FairActMin"]
+      light_act_min = request.form["LightActMin"]
+      seden_act_min = request.form["SedenMin"]
+      print(request.form.get("chosenModel", None))
+      # d=request.form.to_dict(flat=False)
+      
+      try:
          #   if request.form.get("chosenModel", None) = "Model 1"
 
          #TODO: Refactor into a functions
@@ -161,15 +164,21 @@ def send():
             float(fair_act_min),
             float(light_act_min),
             float(seden_act_min)
-            ])
-         model_name = 'calorie_predictor.model'
-         pred_calories = xgboost_model(model_name,results)
-         # output = '{}'.format(pred_calories)
-         output = round(pred_calories[0],2)
-        except:
-           output = 'Error: incorrect input!'
-
-        return render_template("form.html", output=output)
+         ])
+         if request.form.get("chosenModel", None) == "Model 1":
+            model_name = 'calorie_predictor.model'
+            pred_calories = xgboost_model(model_name,results)
+            # output = '{}'.format(pred_calories)
+            output = round(pred_calories[0],2)
+         elif request.form.get("chosenModel", None) == "Model 2":
+            model_name = 'calorie_predictor_SVR.model'
+            scale_name = 'scale_SVR.param'
+            pred_calories = svm_model(model_name, scale_name, results)
+            output = round(pred_calories[0],2)
+      except:
+         output = 'Error: incorrect input!'
+      
+   return render_template("form.html", output=output)
 # 
 
 if __name__ == "__main__":
